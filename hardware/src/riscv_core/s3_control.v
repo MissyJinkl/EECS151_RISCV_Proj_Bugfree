b2fa8490c4ab51c60fe442939f75a169c98633dc
Chenlei Zhu <zhuchenlei@berkeley.edu>
2024-11-20 17:56:47 -0800

    s3 mem control

--- hardware/src/riscv_core/s3_control.v
+++ hardware/src/riscv_core/s3_control.v
@@ -31,33 +31,33 @@ module s3_control(
     always @(*) begin
         case(opcode5)
             `OPC_LUI_5: begin
-                mem_sel = 3'd1;
-                wb_sel = 2'd1;
-                reg_we = 1'b1;
+                mem_sel = 3'd0;
+                wb_sel = 2'd0;
+                reg_we = 1'b0;
             end
             `OPC_AUIPC_5: begin
-                mem_sel = 3'd1;
-                wb_sel = 2'd1;
-                reg_we = 1'b1;
+                mem_sel = 3'd0;
+                wb_sel = 2'd0;
+                reg_we = 1'b0;
             end
             `OPC_JAL_5: begin
-                mem_sel = 3'd1;
-                wb_sel = 2'd2;
-                reg_we = 1'b1;
+                mem_sel = 3'd0;
+                wb_sel = 2'd0;
+                reg_we = 1'b0;
             end
             `OPC_JALR_5: begin
-                mem_sel = 3'd1;
-                wb_sel = 2'd2;
-                reg_we = 1'b1;
+                mem_sel = 3'd0;
+                wb_sel = 2'd0;
+                reg_we = 1'b0;
             end
             `OPC_BRANCH_5: begin
-                mem_sel = 3'd1;
-                wb_sel = 2'd1;
+                mem_sel = 3'd0;
+                wb_sel = 2'd0;
                 reg_we = 1'b0;
             end
             `OPC_STORE_5: begin
-                mem_sel = 3'd1;
-                wb_sel = 2'd1;
+                mem_sel = 3'd0;
+                wb_sel = 2'd0;
                 reg_we = 1'b0;
             end
             `OPC_LOAD_5: begin
@@ -66,14 +66,14 @@ module s3_control(
                 reg_we = 1'b1;
             end
             `OPC_ARI_RTYPE_5: begin
-                mem_sel = 3'd1;
-                wb_sel = 2'd1;
-                reg_we = 1'b1;
+                mem_sel = 3'd0;
+                wb_sel = 2'd0;
+                reg_we = 1'b0;
             end
             `OPC_ARI_ITYPE_5: begin
-                mem_sel = 3'd1;
-                wb_sel = 2'd1;
-                reg_we = 1'b1;
+                mem_sel = 3'd0;
+                wb_sel = 2'd0;
+                reg_we = 1'b0;
             end
             5'b11100: begin
                 mem_sel = 3'd1;
