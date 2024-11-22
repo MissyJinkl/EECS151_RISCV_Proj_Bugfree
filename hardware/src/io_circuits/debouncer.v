6863e0657c491100327b2bea2326f44d6d6a5168
Missy Jin <missy_jin@berkeley.edu>
2024-11-22 15:45:40 -0800

    fix debouncer

--- hardware/src/io_circuits/debouncer.v
+++ hardware/src/io_circuits/debouncer.v
@@ -16,36 +16,42 @@ module debouncer #(
     // You need to think of the conditions for resetting, clock enable, etc.
     // Refer to the block diagram in the spec
 
-    reg [WRAPPING_CNT_WIDTH-1:0] wrapping_counter = {WRAPPING_CNT_WIDTH{1'b0}};
-    wire sample_pulse = (wrapping_counter == SAMPLE_CNT_MAX - 1);
+    
+    reg [WRAPPING_CNT_WIDTH-1:0] wrapping_counter = 0;
+    wire sample_signal;
+    reg [WIDTH-1:0] debounced_signal_reg = 0;
+    reg [SAT_CNT_WIDTH-1:0] saturating_counter [WIDTH-1:0];
 
-    // wrapping_counter
-    always @(posedge clk) begin 
-        if (sample_pulse)
-            wrapping_counter <= {WRAPPING_CNT_WIDTH{1'b0}};
-        else
-            wrapping_counter <= wrapping_counter + 1;
+    always@(posedge clk)begin
+        if(wrapping_counter == SAMPLE_CNT_MAX - 1) wrapping_counter <= 0;
+        else wrapping_counter = wrapping_counter + 1;
     end
 
-    assign debounced_signal = {WIDTH{1'b0}};
-    reg [SAT_CNT_WIDTH-1:0] saturating_counter [WIDTH-1:0];
-    integer i;
+    assign sample_signal = (wrapping_counter == SAMPLE_CNT_MAX - 1);
 
-    always @(posedge clk) begin
-        for (i = 0; i < WIDTH; i = i + 1) begin
-            if (sample_pulse) begin
-                if (glitchy_signal[i]) begin
-                    if (saturating_counter[i] < PULSE_CNT_MAX)
-                        saturating_counter[i] <= saturating_counter[i] + 1;
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin : debounce_logic
+            always @(posedge clk) begin
+                if (sample_signal) begin
+                    if (glitchy_signal[i]) begin
+                        if (saturating_counter[i] < PULSE_CNT_MAX - 1) begin
+                            saturating_counter[i] <= saturating_counter[i] + 1;
+                        end
+                    end else begin
+                        saturating_counter[i] <= 0;
+                    end
+                end
+
+                if (saturating_counter[i] == PULSE_CNT_MAX - 1) begin
+                    debounced_signal_reg[i] <= 1'b1;
                 end else begin
-                    saturating_counter[i] <= {SAT_CNT_WIDTH{1'b0}};
+                    debounced_signal_reg[i] <= 1'b0;
                 end
             end
-
-            if (saturating_counter[i] == PULSE_CNT_MAX)
-                debounced_signal[i] <= 1'b1;
-            else
-                debounced_signal[i] <= 1'b0;
         end
-    end
+    endgenerate
+
+    assign debounced_signal = debounced_signal_reg;
+
 endmodule
